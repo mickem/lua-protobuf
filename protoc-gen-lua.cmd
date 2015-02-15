ab8c1e9b99482a7ca64c3759f423ce053b826850
Michael Medin <michael@medin.name>
2015-02-15 11:55:22 +0100

    Added support for "stand alone" mode

--- protoc-gen-lua.cmd
+++ protoc-gen-lua.cmd
@@ -2,5 +2,12 @@
 #!python.exe
 # EASY-INSTALL-SCRIPT: 'lua-protobuf==0.0.1','protoc-gen-lua'
 __requires__ = 'lua-protobuf==0.0.1'
-import pkg_resources
-pkg_resources.run_script('lua-protobuf==0.0.1', 'protoc-gen-lua')
+try:
+	import pkg_resources
+	pkg_resources.run_script('lua-protobuf==0.0.1', 'protoc-gen-lua')
+except:
+	import sys
+	from os.path import dirname, realpath
+	path = dirname(realpath(__file__))
+	sys.path.append(path)
+	execfile("%s/protoc-gen-lua"%path)
